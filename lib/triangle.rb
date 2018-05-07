99f3e6c57d526aedfa8079864ae9cd3260028175
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-07 20:20:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
   def validate_triangle 
     if @side1 <= 0 || @side2 <=0 || @side3<=0 
        "invalid triangle"
-     elsif (@side1 + @side2 < @side3) || (@side2+@side3<@side1) 
+     elsif (@side1 + @side2 < @side3) || (@side2+@side3<@side1) ||
     
   class TriangleError < StandardError
   end 
