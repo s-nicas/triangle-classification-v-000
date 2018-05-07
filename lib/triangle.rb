75cff1a7b80ceb25ef2d96577823357c3d953870
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-07 20:20:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3,   
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3 
+ 
+  end 
+  
+  def kind
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      :scalene
+     end 
+    
+  end 
+  
+  def validate_triangle 
+    if @side1 <= 0 || @side2 <=0 || @side3<=0 
+       "invalid triangle"
+     elsif (@side1 + @side2 < @side3) || (@side2+@side3<@side1) 
+    
+  class TriangleError < StandardError
+  end 
+    
 end
