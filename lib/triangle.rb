be488d64b6f8ab5dc50b52c46c79e5ccdc6b7ecc
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-07 20:37:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
        raise TriangleError
      elsif (@side1 + @side2 < @side3) || (@side2+@side3<@side1) ||
      (@side3 + @side1 < @side2)
-        raise PartnerError
+        raise TriangleError
       end 
     end 
   class TriangleError < StandardError
